7a133e60618ac148a12d03cbc31595e5c5ddb50d
jimynu <kim@bylr.ch>
2018-02-18 18:24:57 +0100

    yo

--- dbmodels.js
+++ dbmodels.js
@@ -29,7 +29,7 @@ const ConfigSchema = new mongoose.Schema({
   active: Boolean,
   themen: [],
   separat: [],
-  email: String,
+  email: [],
   separat_archiv: []
 })
 
@@ -38,16 +38,13 @@ ConfigSchema.method('archiveSeparat', function(entscheidNr, callback) {
     if ( nr === entscheidNr ) return false;
     return true;
   })
-  console.log(separat);
   let separat_archiv = [...this.separat_archiv];
   separat_archiv.push(entscheidNr);
-  console.log(this);
   Object.assign(
     this,
     { separat: [...separat] },
     { separat_archiv: [...separat_archiv] }
   );
-  console.log(this);
   this.save(callback);
 });
 
